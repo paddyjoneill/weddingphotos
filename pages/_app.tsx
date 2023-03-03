2b7b359541763be60f8c3ce25b447b8439263081
Paddy <patrick@firefinch.io>
2023-03-03 22:24:49 +0000

    add open graph

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { LoggedInContextProvider } from '@/components/contexts/LoggedInContext';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
+    const description = 'Share your pictures with us';
+    const title = "Paddy and Jade's Wedding Photos";
+
     return (
         <LoggedInContextProvider>
+            <Head>
+                <meta name="Description" content={description} />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <title>{title}</title>
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content={title} />
+                <meta property="og:description" content={description} />
+                <meta property="og:image" content="https://weddingphotos.vercel.app/holyisle.webp" />
+            </Head>
             <Component {...pageProps} />
         </LoggedInContextProvider>
     );
